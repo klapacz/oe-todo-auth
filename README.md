b3a02c12ec587a6caf76b3191c48240ee7f7ce9e
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2021-09-13 07:00:34 +0200

    use env vars for db connection

---
+++ README.md
@@ -0,0 +1,9 @@
+## Used env vars
+
+```
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=secret
+POSTGRES_HOST=db
+
+BE_AUTH_DB=auth
+```
--- models/setup.go
+++ models/setup.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,7 +11,12 @@ import (
 var DB *gorm.DB
 
 func SetupDatabase() {
-	dsn := "host=db user=postgres password=secret dbname=auth sslmode=disable"
+	dsn := "host=" + os.Getenv("POSTGRES_HOST") +
+		" user=" + os.Getenv("POSTGRES_USER") +
+		" password=" + os.Getenv("POSTGRES_PASSWORD") +
+		" dbname=" + os.Getenv("BE_AUTH_DB") +
+		" sslmode=disable"
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database %s", err)
